58a9a1ba0de7950e831aa7966c892ed5b58e38ef
Ayush <ayush020yv@gmail.com>
2022-11-08 11:01:31 +0530

    CreateArea

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CreateArea() {
+  return (
+    <div>
+      <form className="create-note">
+        <input type="text" name="title" placeholder="title" />
+      </form>
+      Create Area Component
+    </div>
+  );
+}
+
+export default CreateArea;
